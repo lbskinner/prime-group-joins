6dcce654a8df8f63ccbfe0b833cde8777b67aef5
Leila Skinner <beileiwang@gmail.com>
2020-04-21 14:20:15 -0500

    add imporved solutuion for last stretch

--- joins-solution.sql
+++ joins-solution.sql
@@ -50,3 +50,10 @@ LEFT JOIN "orders" ON "orders".address_id = "addresses".id
 LEFT JOIN "line_items" ON "line_items".order_id = "orders".id
 LEFT JOIN "products"  ON "products".id = "line_items".product_id
 GROUP BY "customers".id;
+
+--or
+SELECT COALESCE(sum("products".unit_price * "line_items".quantity), 0) as "total_order_cost","customers".first_name, "customers".last_name, "customers".id 
+FROM "products" JOIN "line_items" ON "products".id = "line_items".product_id
+JOIN "orders" ON "line_items".order_id = "orders".id
+JOIN "addresses" ON "orders".address_id = "addresses".id
+FULL JOIN "customers" ON "addresses".customer_id = "customers".id GROUP BY "customers".id;
